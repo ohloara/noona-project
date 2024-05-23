68d8851be92a18fd441a3b4d05cb5ef973a1410f
Udemy100 <srk03041@naver.com>
2024-05-23 21:24:59 +0900

    할일앱 만들기 1~4 (2024-05-23)

---
+++ todolist-demo/todo-be/controller/task.controller.js
@@ -0,0 +1,54 @@
+const Task = require("../model/Task");
+const taskController = {};
+
+
+taskController.createTask = async (req, res) =>{
+    try{
+        const {task, isComplete} = req.body;
+        const newTask = new Task({task, isComplete});
+        await newTask.save();
+        res.status(200).json({status:'ok', data:newTask});
+    }catch(err){
+        console.log(err);
+        res.status(400).json({status:'fail', error: err});
+    }
+    
+}
+
+taskController.getTask = async (req, res) =>{
+    try{
+        const taskList = await Task.find({}).select("-__v");
+        res.status(200).json({status:"ok", data: taskList});
+    }catch(err){
+        res.status(400).json({status:"fail", error: err});
+    }
+}
+
+taskController.updateTask = async (req, res) =>{
+    try{
+        const id = req.params.id;
+        const task = await Task.findOne({"_id":id});
+        if(!task){
+            throw new Error("not find task");
+        }
+        const isComplete = task.isComplete;
+        const updateTask = await Task.updateOne(
+            {"_id":id},
+            {$set:{isComplete:!isComplete}}
+        );
+        res.status(200).json({status:"ok", data: updateTask});
+    }catch(err){
+        res.status(400).json({status:"fail", error: err});
+    }
+}
+
+taskController.deleteTask = async (req, res) =>{
+    try{
+        const task = await Task.deleteOne({_id:req.params.id});
+        res.status(200).json({status:"ok", data:task});
+    }catch(err){
+        res.status(400).json({status:"fail", error: err});
+    }
+}
+
+module.exports = taskController;
